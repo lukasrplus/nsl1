fb41e2cf99359ea564de7c8bc7a879322a48230f
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-05-07 14:56:02 +0200

    NSL Page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nsl1::Application.config.secret_key_base = 'b3767c78a4b21f3852351626a3854ea367ae1f4d05b29f9b7132041b2996bd4dcddc850bbe64e4ea489fc1c1a8c9f6cd608d0fd737280f5bc3c021660aadc36d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nsl1::Application.config.session_store :cookie_store, key: '_nsl1_session'
